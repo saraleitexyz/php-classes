c20b987e420a71c420126e433ab13c0d6f29531e
SaraExamen <emailSara@examen.com>
2025-10-23 12:29:23 +0200

    Add 02-for 02.php

---
+++ 02-relaciones-ejercicios/01-bucles/02-for/02.php
@@ -0,0 +1,19 @@
+<!--
+2 - Realizar un programa que imprima en pantalla los números del 20 al 30.
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for ($i = 20; $i <= 30; $i++) {
+        echo $i;
+        echo "<br>";
+    }
+    ?>
+</body>
+</html>
