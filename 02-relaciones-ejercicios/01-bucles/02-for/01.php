2cf3ccdb565f0ba8a54bd789af3a0f2ffd8747d0
SaraExamen <emailSara@examen.com>
2025-10-23 12:28:22 +0200

    Add 02-for 01.php

---
+++ 02-relaciones-ejercicios/01-bucles/02-for/01.php
@@ -0,0 +1,19 @@
+<!--
+1 - Realizar un programa que imprima en pantalla los números del 0 al 100.
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for ($i = 0; $i <= 100; $i++) {
+        echo $i;
+        echo "<br>";
+    }
+    ?>
+</body>
+</html>
