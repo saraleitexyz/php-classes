8909d25b26c66777860d4d01e8c95cb6a70e48c5
SaraExamen <emailSara@examen.com>
2025-10-23 14:54:34 +0200

    Add 02-arrays ex. 231

---
+++ 02-relaciones-ejercicios/02-arrays/231bola8.php
@@ -0,0 +1,7 @@
+<!--
+A partir del anterior, crea un programa que muestre la pregunta recibida 
+y genere una respuesta de manera aleatoria entre un conjunto de respuestas 
+predefinidas, almacenadas en un array: Si, no, quizás, claro que sí, por 
+supuesto que no, no lo tengo claro, seguro, yo diría que sí, ni de coña, etc...
+Este ejercicio se basa en el juego de la Bola 8 mágica.
+-->
