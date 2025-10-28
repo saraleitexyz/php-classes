ff4ff91f89a20d5b15a571e2efd3cc5e4843b5c8
Sara Leite <saraleite.mail@gmail.com>
2025-10-28 23:15:17 +0100

    Add rel02 exercise 231

--- 02-relaciones-ejercicios/02-arrays/231bola8.php
+++ 02-relaciones-ejercicios/02-arrays/231bola8.php
@@ -5,3 +5,12 @@ predefinidas, almacenadas en un array: Si, no, quizás, claro que sí, por
 supuesto que no, no lo tengo claro, seguro, yo diría que sí, ni de coña, etc...
 Este ejercicio se basa en el juego de la Bola 8 mágica.
 -->
+<?php
+    if (isset($_GET['user_question'])) {
+        $user_question = $_GET['user_question'];
+        $answers = ["Sí", "No", "Quizás", "Claro que sí", "Por supuesto que no", "No lo tengo claro", "Seguro", "Yo diría que sí", "Ni de coña"];
+        $random_answer = $answers[array_rand($answers)];
+        echo "Pregunta: $user_question <br />";
+        echo "Respuesta: $random_answer";
+    }
+?>
